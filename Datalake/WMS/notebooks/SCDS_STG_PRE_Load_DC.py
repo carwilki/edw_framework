99bc749f3c73df7623d40bcb8fdfe1629d393290
Carson Wilkins <cwilkins01@petsmart.com>
2023-05-04 13:47:59 -0700

    modified the wkl implementation

--- Datalake/WMS/notebooks/SCDS_STG_PRE_Load_DC.py
+++ Datalake/WMS/notebooks/SCDS_STG_PRE_Load_DC.py
@@ -4,23 +4,21 @@ dbutils.widgets.text(name='Prev_Run_Dt', defaultValue='01/01/1901')
 dbutils.widgets.text(name='Initial_Load', defaultValue='')
 
 # COMMAND ----------
+dbutils.notebook.run('m_WM_Ucl_User_PRE.py','60', {
+                             "DC_NBR":f"{dc}",
+                             "Prev_Run_Dt":f"{dbutils.get('Prev_Run_Dt')}",
+                             "Initial_Load":f"{dbutils.get('Prev_Run_Dt')}"
+                            })
 
-for dc in [10,12,14,22,36,38,41]
-    dbutils.notebook.run('m_WM_Ucl_User_PRE.py','60',\
-    {
-        "DC_NBR":f"{dc}",
-        "Prev_Run_Dt":f"{dbutils.get('Prev_Run_Dt')}",
-        "Initial_Load":f"{dbutils.get('Prev_Run_Dt')}"
-    }
-    dbutils.notebook.run('m_WM_E_Dept_PRE.py','60',\
-    {
-       "DC_NBR":f"{dc}",
-        "Prev_Run_Dt":f"{dbutils.get('Prev_Run_Dt')}",
-        "Initial_Load":f"{dbutils.get('Prev_Run_Dt')}"
-    })
-    dbutils.notebook.run('m_WM_E_Consol_Perf_Smry_PRE.py','60',\
-    {
-        "DC_NBR":f"{dc}",
-        "Prev_Run_Dt":f"{dbutils.get('Prev_Run_Dt')}",
-        "Initial_Load":f"{dbutils.get('Prev_Run_Dt')}"
-})
+dbutils.notebook.run('m_WM_E_Dept_PRE.py','60',
+                            {
+                             "DC_NBR":f"{dc}",
+                             "Prev_Run_Dt":f"{dbutils.get('Prev_Run_Dt')}",
+                             "Initial_Load":f"{dbutils.get('Prev_Run_Dt')}"
+                            })
+
+dbutils.notebook.run('m_WM_E_Consol_Perf_Smry_PRE.py','60', {
+                             "DC_NBR":f"{dc}",
+                             "Prev_Run_Dt":f"{dbutils.get('Prev_Run_Dt')}",
+                             "Initial_Load":f"{dbutils.get('Prev_Run_Dt')}"
+                            })
