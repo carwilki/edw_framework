f431fc862295bca10229e77a33ac1c4e23a40747
pshekhar@petsmart.com <pshekhar@petsmart.com>
2023-05-23 22:26:46 +0000

    add prev_run_dt in wm_e_dept

--- Datalake/WMS/notebooks/m_WM_E_Dept_PRE.py
+++ Datalake/WMS/notebooks/m_WM_E_Dept_PRE.py
@@ -34,8 +34,9 @@ refine_table_name='WM_E_DEPT'
 
 prev_run_dt = str(spark.sql(f"""select max(prev_run_date) from {env}_raw.log_run_details where table_name='{refine_table_name}' and lower(status)= 'completed'""").collect()[0][0])
 
-if prev_run_dt == "None":
-    prev_run_dt = getMaxDate(refine_table_name,env)
+if prev_run_dt == None:
+    #prev_run_dt = getMaxDate(refine_table_name,env)
+    prev_run_dt = "2000-01-01"
 else:
     prev_run_dt = datetime.strptime(prev_run_dt, "%Y-%m-%d %H:%M:%S")
     prev_run_dt = prev_run_dt.strftime('%Y-%m-%d')
