681436ceeddf771225a19877d44dddecd06fe00d
rsingha@petsmart.com <rsingha@petsmart.com>
2023-07-05 11:55:14 +0000

    testing

--- Datalake/WMS/WMS_TO_SCDS_DAILY/debug_pre_refine.py
+++ Datalake/WMS/WMS_TO_SCDS_DAILY/debug_pre_refine.py
@@ -1,11 +1,6 @@
 # Databricks notebook source
-from Datalake.WMS.WMS_TO_SCDS_DAILY.notebooks.m_WM_Yard_Zone_PRE import *
+perf_smry("dc10","qa")
 
 # COMMAND ----------
 
-m_WM_Yard_Zone_PRE("dc10","qa")
 
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select *
