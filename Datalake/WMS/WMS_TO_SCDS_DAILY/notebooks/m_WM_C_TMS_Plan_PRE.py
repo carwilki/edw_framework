abfb42d9176b3e3087737f4012f50323874c2c9c
rsingha <rsingha@petsmart.com>
2023-07-07 21:20:58 +0530

    WM_C_TMS_Plan_PRE  another timestamp fix

--- Datalake/WMS/WMS_TO_SCDS_DAILY/notebooks/m_WM_C_TMS_Plan_PRE.py
+++ Datalake/WMS/WMS_TO_SCDS_DAILY/notebooks/m_WM_C_TMS_Plan_PRE.py
@@ -93,7 +93,7 @@ def m_WM_C_TMS_Plan_PRE(dcnbr, env):
                 C_TMS_PLAN.DEL_TYPE,
                 C_TMS_PLAN.TMS_TRAILER_ID,
                 C_TMS_PLAN.CARRIER_REF_NUM,
-                cast(C_TMS_PLAN.TMS_PLAN_TIME as timestamp),
+                cast(C_TMS_PLAN.TMS_PLAN_TIME as timestamp) as TMS_PLAN_TIME,
                 C_TMS_PLAN.PLT_WEIGHT,
                 C_TMS_PLAN.ERROR_SEQ_NBR,
                 C_TMS_PLAN.STAT_CODE,
