1720a63bf3152c9b772b32317487c8425e9d9233
pshekhar2308 <124131160+pshekhar2308@users.noreply.github.com>
2023-07-09 15:30:05 -0700

    added possibles/globals in preTable_func call

--- Datalake/WMS/WMS_TO_SCDS_DAILY/notebooks/WL_SCDS_STG_PRE_LOAD_DC.py
+++ Datalake/WMS/WMS_TO_SCDS_DAILY/notebooks/WL_SCDS_STG_PRE_LOAD_DC.py
@@ -427,33 +427,51 @@ set_A_I_lst = [
 def tableGroupsNfunc(dcnbr, env, setNo):
     if setNo == "set_A_I_1":
         for table in set_A_I_lst[:18]:
-            preTable_func = "m_".join(table).join("_PRE")
-            eval(preTable_func(dcnbr, env))
+            preTable_method_name = "m_".join(table).join("_PRE")
+            possibles = globals().copy()
+            possibles.update(locals())
+            preTable_func = possibles.get(preTable_method_name)
+            preTable_func(dcnbr, env)
             logger.info(f"{0} executed".format(preTable_func))
     elif setNo == "set_A_I_2":
         for table in set_A_I_lst[18:36]:
-            preTable_func = "m_".join(table).join("_PRE")
-            eval(preTable_func(dcnbr, env))
+            preTable_method_name = "m_".join(table).join("_PRE")
+            possibles = globals().copy()
+            possibles.update(locals())
+            preTable_func = possibles.get(preTable_method_name)
+            preTable_func(dcnbr, env)
             logger.info(f"{0} executed".format(preTable_func))
     elif setNo == "set_I_P_1":
         for table in set_I_P_lst[:18]:
-            preTable_func = "m_".join(table).join("_PRE")
-            eval(preTable_func(dcnbr, env))
+            preTable_method_name = "m_".join(table).join("_PRE")
+            possibles = globals().copy()
+            possibles.update(locals())
+            preTable_func = possibles.get(preTable_method_name)
+            preTable_func(dcnbr, env)
             logger.info(f"{0} executed".format(preTable_func))
     elif setNo == "set_I_P_2":
         for table in set_I_P_lst[18:36]:
-            preTable_func = "m_".join(table).join("_PRE")
-            eval(preTable_func(dcnbr, env))
+            preTable_method_name = "m_".join(table).join("_PRE")
+            possibles = globals().copy()
+            possibles.update(locals())
+            preTable_func = possibles.get(preTable_method_name)
+            preTable_func(dcnbr, env)
             logger.info(f"{0} executed".format(preTable_func))
     elif setNo == "set_P_Y_1":
         for table in set_I_P_lst[:18]:
-            preTable_func = "m_".join(table).join("_PRE")
-            eval(preTable_func(dcnbr, env))
+            preTable_method_name = "m_".join(table).join("_PRE")
+            possibles = globals().copy()
+            possibles.update(locals())
+            preTable_func = possibles.get(preTable_method_name)
+            preTable_func(dcnbr, env)
             logger.info(f"{0} executed".format(preTable_func))
     else:
         for table in set_I_P_lst[18:36]:
-            preTable_func = "m_".join(table).join("_PRE")
-            eval(preTable_func(dcnbr, env))
+            preTable_method_name = "m_".join(table).join("_PRE")
+            possibles = globals().copy()
+            possibles.update(locals())
+            preTable_func = possibles.get(preTable_method_name)
+            preTable_func(dcnbr, env)
             logger.info(f"{0} executed".format(preTable_func))
 
 
