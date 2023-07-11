247444993a945ca0ecca76777c038415798561cc
pshekhar2308 <124131160+pshekhar2308@users.noreply.github.com>
2023-07-10 23:46:06 -0700

    Updated SQ_Shortcut_to_RACK_TYPE_LEVEL

--- Datalake/WMS/WMS_TO_SCDS_DAILY/notebooks/m_WM_Rack_Type_Level_PRE.py
+++ Datalake/WMS/WMS_TO_SCDS_DAILY/notebooks/m_WM_Rack_Type_Level_PRE.py
@@ -1,4 +1,4 @@
-#Code converted on 2023-06-22 20:25:06
+# Code converted on 2023-06-22 20:25:06
 import os
 import argparse
 from pyspark.sql import *
@@ -13,37 +13,35 @@ from Datalake.utils.mergeUtils import *
 from Datalake.utils.logger import *
 
 
-
 def m_WM_Rack_Type_Level_PRE(dcnbr, env):
     from logging import getLogger, INFO
 
     logger = getLogger()
     spark: SparkSession = SparkSession.getActiveSession()
     logger.info("inside m_WM_Rack_Type_Level_PRE")
-    
+
     if dcnbr is None or dcnbr == "":
         raise ValueError("DC_NBR is not set")
     if env is None or env == "":
         raise ValueError("env is not set")
-    
+
     refine = getEnvPrefix(env) + "refine"
     raw = getEnvPrefix(env) + "raw"
-    
+
     tableName = "WM_RACK_TYPE_LEVEL_PRE"
     schemaName = raw
     source_schema = "WMSMIS"
 
-    
     target_table_name = schemaName + "." + tableName
     refine_table_name = tableName[:-4]
-    Prev_Run_Dt=genPrevRunDt(refine_table_name, refine,raw)
+    Prev_Run_Dt = genPrevRunDt(refine_table_name, refine, raw)
     print("The prev run date is " + Prev_Run_Dt)
-    
+
     (username, password, connection_string) = getConfig(dcnbr, env)
     logger.info("username, password, connection_string is obtained from getConfig fun")
-    
+
     dcnbr = dcnbr.strip()[2:]
-    
+
     query = f"""SELECT
                     RACK_TYPE_LEVEL.RACK_LEVEL_ID,
                     RACK_TYPE_LEVEL.RACK_TYPE,
@@ -110,92 +108,97 @@ def m_WM_Rack_Type_Level_PRE(dcnbr, env):
                     RACK_TYPE_LEVEL.MOVE_GUIDES_LFT
                 FROM {source_schema}.RACK_TYPE_LEVEL
                 WHERE  (TRUNC(CREATE_DATE_TIME) >= TRUNC(to_date('{Prev_Run_Dt}','YYYY-MM-DD'))-14) OR (TRUNC(MOD_DATE_TIME) >=  TRUNC(to_date('{Prev_Run_Dt}','YYYY-MM-DD'))-14)"""
-                        
 
-    spark.read.format('jdbc').option('url', connection_string).option( = jdbcOracleConnection(query, username, password, connection_string).withColumn("sys_row_id", monotonically_increasing_id())
-    logger.info("SQL query for spark.read.format('jdbc').option('url', connection_string).option( is executed and data is loaded using jdbc")
-    
-    
-    # Processing node EXPTRANS, type EXPRESSION 
-    # COLUMN COUNT: 65
-    
-    # for each involved DataFrame, append the dataframe name to each column
-    SQ_Shortcut_to_RACK_TYPE_LEVEL_temp = SQ_Shortcut_to_RACK_TYPE_LEVEL.toDF(*["SQ_Shortcut_to_RACK_TYPE_LEVEL___" + col for col in SQ_Shortcut_to_RACK_TYPE_LEVEL.columns])
-    
-    EXPTRANS = SQ_Shortcut_to_RACK_TYPE_LEVEL_temp.selectExpr( 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___sys_row_id as sys_row_id", 
-        f"{dcnbr} as DC_NBR_exp", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RACK_LEVEL_ID as RACK_LEVEL_ID", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RACK_TYPE as RACK_TYPE", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_NUMBER as LEVEL_NUMBER", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_WT_LIMIT as LEVEL_WT_LIMIT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_HT as LEVEL_HT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_WIDTH as LEVEL_WIDTH", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_DEPTH as LEVEL_DEPTH", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___NBR_OF_SLOTS as NBR_OF_SLOTS", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SLOT_WT_LIMIT as SLOT_WT_LIMIT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_LANE_WT as MAX_LANE_WT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___HT_CLEARANCE as HT_CLEARANCE", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SIDE_CLEAR as SIDE_CLEAR", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_STACKING as MAX_STACKING", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_LANES as MAX_LANES", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___GUIDE_WIDTH as GUIDE_WIDTH", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LANE_GUIDES as LANE_GUIDES", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SLOT_GUIDES as SLOT_GUIDES", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MOVE_GUIDES as MOVE_GUIDES", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___USE_FULL_WID as USE_FULL_WID", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SHELF_THICKNESS as SHELF_THICKNESS", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LABEL_POS as LABEL_POS", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_PICK_HT as MAX_PICK_HT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SLOT_UNITS as SLOT_UNITS", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SHIP_UNITS as SHIP_UNITS", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_SL_WID as MAX_SL_WID", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___ONE_FACING_PER_DSW as ONE_FACING_PER_DSW", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_PRIORITY as LEVEL_PRIORITY", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___COST_TO_PICK as COST_TO_PICK", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RESERVED_1 as RESERVED_1", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RESERVED_2 as RESERVED_2", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RESERVED_3 as RESERVED_3", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RESERVED_4 as RESERVED_4", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___CREATE_DATE_TIME as CREATE_DATE_TIME", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MOD_DATE_TIME as MOD_DATE_TIME", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MOD_USER as MOD_USER", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___USE_INCREMENT as USE_INCREMENT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___WIDTH_INCREMENT as WIDTH_INCREMENT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MIN_SL_WIDTH as MIN_SL_WIDTH", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MIN_ITEM_WIDTH as MIN_ITEM_WIDTH", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_OVERHANG_PCT as MAX_OVERHANG_PCT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEV_MAP_STR as LEV_MAP_STR", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___POS_MAP_STR as POS_MAP_STR", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___USE_LEV_MAP_STR as USE_LEV_MAP_STR", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___USE_POS_MAP_STR as USE_POS_MAP_STR", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___USE_VERT_DIV as USE_VERT_DIV", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___VERT_DIV_HT as VERT_DIV_HT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MIN_CHANNEL_WID as MIN_CHANNEL_WID", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_CHANNEL_WID as MAX_CHANNEL_WID", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_CHANNEL_WT as MAX_CHANNEL_WT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_ADJ_SLOTS as MAX_ADJ_SLOTS", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_EJECTOR_WT as MAX_EJECTOR_WT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_ROTATE as AFRAME_ROTATE", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MIN_HT as AFRAME_MIN_HT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MAX_HT as AFRAME_MAX_HT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MIN_LEN as AFRAME_MIN_LEN", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MAX_LEN as AFRAME_MAX_LEN", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MIN_WID as AFRAME_MIN_WID", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MAX_WID as AFRAME_MAX_WID", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MIN_WT as AFRAME_MIN_WT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MAX_WT as AFRAME_MAX_WT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___REACH_DIST as REACH_DIST", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MOVE_GUIDES_RGT as MOVE_GUIDES_RGT", 
-        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MOVE_GUIDES_LFT as MOVE_GUIDES_LFT", 
-        "CURRENT_TIMESTAMP() as LOAD_TSTMP" 
+    SQ_Shortcut_to_RACK_TYPE_LEVEL = jdbcOracleConnection(
+        query, username, password, connection_string
+    ).withColumn("sys_row_id", monotonically_increasing_id())
+    logger.info(
+        "SQL query for spark.read.format('jdbc').option('url', connection_string).option( is executed and data is loaded using jdbc"
     )
-    
-    
-    # Processing node Shortcut_to_WM_RACK_TYPE_LEVEL_PRE, type TARGET 
+
+    # Processing node EXPTRANS, type EXPRESSION
     # COLUMN COUNT: 65
-    
-    
+
+    # for each involved DataFrame, append the dataframe name to each column
+    SQ_Shortcut_to_RACK_TYPE_LEVEL_temp = SQ_Shortcut_to_RACK_TYPE_LEVEL.toDF(
+        *[
+            "SQ_Shortcut_to_RACK_TYPE_LEVEL___" + col
+            for col in SQ_Shortcut_to_RACK_TYPE_LEVEL.columns
+        ]
+    )
+
+    EXPTRANS = SQ_Shortcut_to_RACK_TYPE_LEVEL_temp.selectExpr(
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___sys_row_id as sys_row_id",
+        f"{dcnbr} as DC_NBR_exp",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RACK_LEVEL_ID as RACK_LEVEL_ID",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RACK_TYPE as RACK_TYPE",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_NUMBER as LEVEL_NUMBER",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_WT_LIMIT as LEVEL_WT_LIMIT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_HT as LEVEL_HT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_WIDTH as LEVEL_WIDTH",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_DEPTH as LEVEL_DEPTH",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___NBR_OF_SLOTS as NBR_OF_SLOTS",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SLOT_WT_LIMIT as SLOT_WT_LIMIT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_LANE_WT as MAX_LANE_WT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___HT_CLEARANCE as HT_CLEARANCE",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SIDE_CLEAR as SIDE_CLEAR",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_STACKING as MAX_STACKING",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_LANES as MAX_LANES",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___GUIDE_WIDTH as GUIDE_WIDTH",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LANE_GUIDES as LANE_GUIDES",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SLOT_GUIDES as SLOT_GUIDES",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MOVE_GUIDES as MOVE_GUIDES",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___USE_FULL_WID as USE_FULL_WID",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SHELF_THICKNESS as SHELF_THICKNESS",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LABEL_POS as LABEL_POS",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_PICK_HT as MAX_PICK_HT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SLOT_UNITS as SLOT_UNITS",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___SHIP_UNITS as SHIP_UNITS",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_SL_WID as MAX_SL_WID",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___ONE_FACING_PER_DSW as ONE_FACING_PER_DSW",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEVEL_PRIORITY as LEVEL_PRIORITY",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___COST_TO_PICK as COST_TO_PICK",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RESERVED_1 as RESERVED_1",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RESERVED_2 as RESERVED_2",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RESERVED_3 as RESERVED_3",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___RESERVED_4 as RESERVED_4",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___CREATE_DATE_TIME as CREATE_DATE_TIME",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MOD_DATE_TIME as MOD_DATE_TIME",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MOD_USER as MOD_USER",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___USE_INCREMENT as USE_INCREMENT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___WIDTH_INCREMENT as WIDTH_INCREMENT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MIN_SL_WIDTH as MIN_SL_WIDTH",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MIN_ITEM_WIDTH as MIN_ITEM_WIDTH",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_OVERHANG_PCT as MAX_OVERHANG_PCT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___LEV_MAP_STR as LEV_MAP_STR",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___POS_MAP_STR as POS_MAP_STR",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___USE_LEV_MAP_STR as USE_LEV_MAP_STR",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___USE_POS_MAP_STR as USE_POS_MAP_STR",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___USE_VERT_DIV as USE_VERT_DIV",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___VERT_DIV_HT as VERT_DIV_HT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MIN_CHANNEL_WID as MIN_CHANNEL_WID",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_CHANNEL_WID as MAX_CHANNEL_WID",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_CHANNEL_WT as MAX_CHANNEL_WT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_ADJ_SLOTS as MAX_ADJ_SLOTS",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MAX_EJECTOR_WT as MAX_EJECTOR_WT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_ROTATE as AFRAME_ROTATE",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MIN_HT as AFRAME_MIN_HT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MAX_HT as AFRAME_MAX_HT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MIN_LEN as AFRAME_MIN_LEN",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MAX_LEN as AFRAME_MAX_LEN",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MIN_WID as AFRAME_MIN_WID",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MAX_WID as AFRAME_MAX_WID",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MIN_WT as AFRAME_MIN_WT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___AFRAME_MAX_WT as AFRAME_MAX_WT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___REACH_DIST as REACH_DIST",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MOVE_GUIDES_RGT as MOVE_GUIDES_RGT",
+        "SQ_Shortcut_to_RACK_TYPE_LEVEL___MOVE_GUIDES_LFT as MOVE_GUIDES_LFT",
+        "CURRENT_TIMESTAMP() as LOAD_TSTMP",
+    )
+
+    # Processing node Shortcut_to_WM_RACK_TYPE_LEVEL_PRE, type TARGET
+    # COLUMN COUNT: 65
+
     Shortcut_to_WM_RACK_TYPE_LEVEL_PRE = EXPTRANS.selectExpr(
         "CAST(DC_NBR_exp AS SMALLINT) as DC_NBR",
         "CAST(RACK_LEVEL_ID AS BIGINT) as RACK_LEVEL_ID",
@@ -261,7 +264,12 @@ def m_WM_Rack_Type_Level_PRE(dcnbr, env):
         "CAST(REACH_DIST AS DECIMAL(13,4)) as REACH_DIST",
         "CAST(MOVE_GUIDES_RGT AS TINYINT) as MOVE_GUIDES_RGT",
         "CAST(MOVE_GUIDES_LFT AS TINYINT) as MOVE_GUIDES_LFT",
-        "CAST(LOAD_TSTMP AS TIMESTAMP) as LOAD_TSTMP"
-    )    
-    overwriteDeltaPartition(Shortcut_to_WM_RACK_TYPE_LEVEL_PRE, "DC_NBR", dcnbr, target_table_name)
-    logger.info("Shortcut_to_WM_RACK_TYPE_LEVEL_PRE is written to the target table - " + target_table_name)
+        "CAST(LOAD_TSTMP AS TIMESTAMP) as LOAD_TSTMP",
+    )
+    overwriteDeltaPartition(
+        Shortcut_to_WM_RACK_TYPE_LEVEL_PRE, "DC_NBR", dcnbr, target_table_name
+    )
+    logger.info(
+        "Shortcut_to_WM_RACK_TYPE_LEVEL_PRE is written to the target table - "
+        + target_table_name
+    )
