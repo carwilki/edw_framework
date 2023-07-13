7efc8ac3dbf45daecd2c01e9e43b07c64b459550
pshekhar2308 <124131160+pshekhar2308@users.noreply.github.com>
2023-07-12 23:54:06 -0700

    Updated DeltaLAkeWriter

--- Datalake/utils/DeltaLakeWriter.py
+++ Datalake/utils/DeltaLakeWriter.py
@@ -2,7 +2,6 @@ class DeltaLakeWriter:
     def __init__(self, env, sfOptions, table, primary_keys=None):
         from pyspark.sql import SparkSession
         from Datalake.utils.genericUtilities import getSFEnvSuffix
-        
 
         spark = SparkSession.getActiveSession()
         spark: SparkSession = SparkSession.getActiveSession()
@@ -14,51 +13,61 @@ class DeltaLakeWriter:
         self.primary_keys = primary_keys
         self.env = env
         self.sfOptions = sfOptions
-       
-        
-    def logRun(self, process, table,sf_row_count,delta_row_count,status,error,logTableName):
+
+    def logRun(
+        self, process, table, sf_row_count, delta_row_count, status, error, logTableName
+    ):
         from logging import getLogger, INFO
         import json
+
         logger = getLogger()
         from datetime import datetime as dt
+
         # Getting current date and time
         now = dt.now()
-
 
         s = now.strftime("%Y-%m-%d %H:%M:%S")
         s = str(s)
 
-        context_str = self.dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson()
+        context_str = (
+            self.dbutils.notebook.entry_point.getDbutils()
+            .notebook()
+            .getContext()
+            .toJson()
+        )
         context = json.loads(context_str)
-        task_name = context.get('tags', {}).get('taskKey', None)
-        job_id = context.get('tags', {}).get('jobId', None)
-        run_id_obj = context.get('currentRunId', {})
-        run_id = run_id_obj.get('id', None) if run_id_obj else None
+        task_name = context.get("tags", {}).get("taskKey", None)
+        job_id = context.get("tags", {}).get("jobId", None)
+        run_id_obj = context.get("currentRunId", {})
+        run_id = run_id_obj.get("id", None) if run_id_obj else None
 
-        if task_name  or job_id or run_id is None:
-            task_name='null'
-            job_id=run_id=1
-    
+        if task_name or job_id or run_id is None:
+            task_name = "null"
+            job_id = run_id = 1
 
-        if status.lower()=='failed':
-            logger.info("Inserting failed run details into historical_run_details_from_sf table")
+        if status.lower() == "failed":
+            logger.info(
+                "Inserting failed run details into historical_run_details_from_sf table"
+            )
             sql_query = f"""
             INSERT INTO {logTableName}
             (job_id, run_id, task_name,  process, table_name,sf_rowCount, delta_rowCount,  status, error, run_date) VALUES
             ('{job_id}', '{run_id}', '{task_name}', '{process}', '{table}', '{sf_row_count}', '{delta_row_count}', '{status}', '{error}', '{s}')
          """
         else:
-            logger.info("Inserting success run details into historical_run_details_from_sf table")
+            logger.info(
+                "Inserting success run details into historical_run_details_from_sf table"
+            )
             sql_query = f"""
             INSERT INTO {logTableName}
             (job_id, run_id, task_name,  process, table_name,sf_rowCount, delta_rowCount, status, error, run_date) VALUES
             ('{job_id}', '{run_id}', '{task_name}', '{process}', '{table}', '{sf_row_count}', '{delta_row_count}', '{status}', '{error}', '{s}')
             """
 
-        logger.info('Logging the status')    
+        logger.info("Logging the status")
         logger.info(sql_query)
         spark.sql(sql_query)
-        logger.info('Logging Completed')
+        logger.info("Logging Completed")
 
     def ingestFromSF(self):
         from logging import getLogger, INFO
@@ -76,29 +85,29 @@ class DeltaLakeWriter:
             )
             sf_row_count = df.count()
             delta_row_count = spark.sql(
-            f"select count(*) from {0}.{1}".format(schemaForDeltaTable, self.table)
+                f"select count(*) from {0}.{1}".format(schemaForDeltaTable, self.table)
             )
             if sf_row_count == delta_row_count:
                 logger.info("All records have been ingested to delta lake")
             else:
                 logger.info("Records mismatch")
-            logRun(
-                "Delta Writer -" + self.table,
-                self.table,
-                sf_row_count,
-                delta_row_count,
-                "Succeeded",
-                NULL,
-                f"{schemaForDeltaTable}.historical_run_details_from_sf",
+            self.logRun(
+                process="Delta Writer -" + self.table,
+                table=self.table,
+                sf_row_count=sf_row_count,
+                delta_row_count=delta_row_count,
+                status="Succeeded",
+                error=None,
+                logTableName=f"{schemaForDeltaTable}.historical_run_details_from_sf",
             )
         except Exception as e:
-            logRun(
-                "Delta Writer -" + self.table,
-                self.table,
-                sf_row_count=None
-                delta_row_count=None
-                "Failed",
-                str(e),
-                f"{schemaForDeltaTable}.historical_run_details_from_sf",
+            self.logRun(
+                process="Delta Writer -" + self.table,
+                table=self.table,
+                sf_row_count=None,
+                delta_row_count=None,
+                status="Failed",
+                error=str(e),
+                logTableName=f"{schemaForDeltaTable}.historical_run_details_from_sf",
             )
             raise e
