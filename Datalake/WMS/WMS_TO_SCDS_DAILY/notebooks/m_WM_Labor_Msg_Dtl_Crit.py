0594ec40a17286a45016a1b8bf1e68b7926592ea
rsingha <rsingha@petsmart.com>
2023-07-17 18:27:57 +0530

    col added in few more notebooks

--- Datalake/WMS/WMS_TO_SCDS_DAILY/notebooks/m_WM_Labor_Msg_Dtl_Crit.py
+++ Datalake/WMS/WMS_TO_SCDS_DAILY/notebooks/m_WM_Labor_Msg_Dtl_Crit.py
@@ -253,7 +253,7 @@ UPD_INS_UPD = EXP_UPD_VALIDATOR_temp.selectExpr( \
 	"EXP_UPD_VALIDATOR___LOAD_TSTMP as LOAD_TSTMP", \
 	"EXP_UPD_VALIDATOR___o_UPDATE_VALIDATOR as o_UPDATE_VALIDATOR", \
 	"EXP_UPD_VALIDATOR___LABOR_MSG_DTL_CRIT_ID as LABOR_MSG_DTL_CRIT_ID1") \
-	.withColumn('pyspark_data_action', when(EXP_UPD_VALIDATOR.o_UPDATE_VALIDATOR ==(lit(1)), lit(0)).when(EXP_UPD_VALIDATOR.o_UPDATE_VALIDATOR ==(lit(2)), lit(1)))
+	.withColumn('pyspark_data_action', when(col('o_UPDATE_VALIDATOR') ==(lit(1)), lit(0)).when(col('o_UPDATE_VALIDATOR') ==(lit(2)), lit(1)))
 
 # COMMAND ----------
 # Processing node Shortcut_to_WM_LABOR_MSG_DTL_CRIT1, type TARGET 
