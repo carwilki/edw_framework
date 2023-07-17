40fc81dd12ada5a92aca0779e4b1b073b6032255
Pavan Kulkarni <pkulkarni@petsmart.com>
2023-07-17 10:38:38 -0400

    DDT1-1564 adding additional_config column to the SQL stmt

--- Datalake/Production/rocky/table_ddls/rocky_metadata_petm_rating.sql
+++ Datalake/Production/rocky/table_ddls/rocky_metadata_petm_rating.sql
@@ -11,7 +11,7 @@
 -- MAGIC target_db, target_schema, target_table_name, load_type, source_delta_column,
 -- MAGIC primary_key, initial_load_filter, load_frequency, load_cron_expr, tidal_dependencies,
 -- MAGIC expected_start_time, job_watchers, max_retry, job_tag, is_scheduled,
--- MAGIC job_id, snowflake_ddl , tidal_trigger_condition , disable_no_record_failure , snowflake_pre_sql , snowflake_post_sql 
+-- MAGIC job_id, snowflake_ddl , tidal_trigger_condition , disable_no_record_failure , snowflake_pre_sql , snowflake_post_sql, additional_config
 -- MAGIC )
 -- MAGIC VALUES (
 -- MAGIC "NZ_Migration", null, "NZ_Mako8", "EDW_PRD", "PETM_RATING",
@@ -19,5 +19,5 @@
 -- MAGIC "legacy", "public", "petm_rating", "full", null, 
 -- MAGIC null, null, "daily", null, array("DUMMY_TIDAL_JOB"), 
 -- MAGIC null, array("pkulkarni@petsmart.com"), 1, null, False,
--- MAGIC null, null ,"ALL_MUST_BE_MET" ,null ,null , null
+-- MAGIC null, null ,"ALL_MUST_BE_MET" ,null ,null , null, null
 -- MAGIC );
