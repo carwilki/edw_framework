62d34d653d6835e1d3d148d608f034ae412dec1d
Pavan Kulkarni <pkulkarni@petsmart.com>
2023-07-17 08:49:17 -0400

    DDT-1564 adding Rocky SQL insert stmts for the tables required by wf_prism_daily

---
+++ Datalake/Production/rocky/table_ddls/rocky_metadata_timezone_conversion.sql
@@ -0,0 +1,23 @@
+-- Databricks notebook source
+-- JOB ID : 522940652289096
+-- TIDAL : ETL_BD-Rocky_timezone_conversion_load_DY
+
+-- COMMAND ----------
+
+-- MAGIC %sql
+-- MAGIC INSERT INTO qa_work.rocky_ingestion_metadata (
+-- MAGIC table_group, table_group_desc, source_type, source_db, source_table, 
+-- MAGIC table_desc, is_pii, pii_type, has_hard_deletes, target_sink, 
+-- MAGIC target_db, target_schema, target_table_name, load_type, source_delta_column,
+-- MAGIC primary_key, initial_load_filter, load_frequency, load_cron_expr, tidal_dependencies,
+-- MAGIC expected_start_time, job_watchers, max_retry, job_tag, is_scheduled,
+-- MAGIC job_id, snowflake_ddl , tidal_trigger_condition , disable_no_record_failure , snowflake_pre_sql , snowflake_post_sql 
+-- MAGIC )
+-- MAGIC VALUES (
+-- MAGIC "NZ_Migration", null, "NZ_Mako4", "EDW_PRD", "TIMEZONE_CONVERSION", 
+-- MAGIC null, false, null, false, "delta",
+-- MAGIC "legacy", "public", "timezone_conversion", "full", null, 
+-- MAGIC null, null, "daily", null, array("DUMMY_TIDAL_JOB"), 
+-- MAGIC null, array("pkulkarni@petsmart.com"), 1, null, false,
+-- MAGIC null, null ,"ALL_MUST_BE_MET" ,null ,null , null
+-- MAGIC );
