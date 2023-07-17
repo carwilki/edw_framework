16844920b41dba3c70c1c30464319b29c8fd9fc3
Carson Wilkins <cwilkins01@petsmart.com>
2023-07-17 14:33:33 -0700

    adds back off/retry logic to getting secrets throught a helper function

---
+++ Datalake/utils/secrets.py
@@ -0,0 +1,15 @@
+from retry import retry
+from py4j.protocol import Py4JJavaError
+from pyspark.sql import SparkSession
+from pyspark.dbutils import DBUtils
+
+sc = SparkSession.getActiveSession()
+dbutils = DBUtils(sc)
+
+
+@retry(tries=10, delay=1, backoff=2, jitter=(1, 5), exceptions=Py4JJavaError)
+def get(scope: str, name: str):
+    """
+    implements backoff and retry logic for dbutils.secrets.get
+    """
+    return dbutils.secrets.get(scope=scope, name=name)
