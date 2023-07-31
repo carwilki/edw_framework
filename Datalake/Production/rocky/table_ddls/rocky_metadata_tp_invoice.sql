7b544be9db64716be22dd14216475a6b340aa6fa
pkulkarni@petssmart.com <pkulkarni@petssmart.com>
2023-07-31 13:57:01 +0000

    DDT1-1838 Prod readiness and updating to upsert strategy

--- Datalake/Production/rocky/table_ddls/rocky_metadata_tp_invoice.sql
+++ Datalake/Production/rocky/table_ddls/rocky_metadata_tp_invoice.sql
@@ -1,5 +1,5 @@
 -- Databricks notebook source
- INSERT INTO qa_work.rocky_ingestion_metadata (
+ INSERT INTO work.rocky_ingestion_metadata (
  table_group, table_group_desc, source_type, source_db, source_table, 
  table_desc, is_pii, pii_type, has_hard_deletes, target_sink, 
  target_db, target_schema, target_table_name, load_type, source_delta_column,
@@ -8,10 +8,10 @@
  job_id, snowflake_ddl , tidal_trigger_condition , disable_no_record_failure , snowflake_pre_sql , snowflake_post_sql ,additional_config
  )
 VALUES (
-"NZ_Migration", null, "NZ_Mako4", "EDW_PRD", "TP_INVOICE",
+"NZ_Migration", null, "NZ_Mako8", "EDW_PRD", "TP_INVOICE",
 null, false, null, false, "delta",
-"refine", "public", "tp_invoice", "full", null, 
-null, null, "daily", null, array("DUMMY_TIDAL_JOB"), 
-null, array("pkulkarni@petsmart.com"), 1, null, False,
+"refine", "public", "tp_invoice", "upsert", "load_dt", 
+"TP_DAY_DT,LOCATION_ID,TP_INVOICE_NBR", null, "daily", null, array("DUMMY_TIDAL_JOB"), 
+null, array("pkulkarni@petsmart.com", "DL_BIG_DATA_OPERATIONS@PetSmart.com"), 1, null, False,
 null, null ,"ALL_MUST_BE_MET" ,null ,null , null, null
 );
