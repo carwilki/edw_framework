f0463e8c71902151953645e0765b71264f570d13
pshekhar2308 <124131160+pshekhar2308@users.noreply.github.com>
2023-08-13 02:39:03 -0700

    pushed triggerRockyJobsInBulk.py

---
+++ Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/sf2delta_metadata_tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE raw.historical_run_details_from_sf (
+  job_id STRING,
+  run_id STRING,
+  task_name STRING,
+  process STRING,
+  table_name STRING,
+  sf_rowCount STRING,
+  delta_rowCount STRING,
+  status STRING,
+  error STRING,
+  run_date TIMESTAMP)
+USING delta;
+
+
+CREATE TABLE raw.sf2delta_pkey_tstcols (
+  tableName STRING,
+  pKeys STRING,
+  tstmp_cols STRING,
+  tstmp_cols1 STRING)
+USING delta;
+
+insert into raw.sf2delta_pKey_tstcols select * from dev_raw.sf2delta_pkey_tstcols;
---
+++ Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/triggerRockyJobsInBulk.py
@@ -0,0 +1,61 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #### TO DO
+# MAGIC ###### --- Update Prod token
+# MAGIC ###### --- Also update the path to the Rocky config csv file
+
+# COMMAND ----------
+
+import json
+import requests
+
+env = "qa"
+if env == 'prod':
+  work_db = 'work'
+  platform_db = 'stranger_things'
+  token = dbutils.secrets.get(scope = "db-token-jobsapi", key = "password")
+  instance_id = "143446717932641.1.gcp.databricks.com"
+else:
+  work_db = 'qa_work'
+  platform_db = 'qa_stranger_things'
+  token = 'dapi2577d49a1c78bd890daa9daaad0cd342'
+  instance_id = "3986616729757273.3.gcp.databricks.com"
+
+def trigger_rocky_job(payload):
+  api_version = '/api/2.1'
+  api_command = '/jobs/run-now'
+  url = f"https://{instance_id}{api_version}{api_command}"
+
+  params = {
+    "Authorization" : "Bearer " + token,
+    "Content-Type" : "application/json"
+  }
+
+  response = requests.post(
+    url = url,
+    headers = params,
+    data = payload
+  )
+
+  return response.text
+
+import csv
+with open('/Workspace/Repos/pshekhar@petsmart.com/nz-databricks-migration/Datalake/WMS/WMS_TO_SCDS_DAILY/tables/wms_scds_daily_rocky_config.csv', newline='') as csvfile:
+  reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+  for row in reader:
+    table = row[4]
+    print(table)
+    job_id = spark.sql(f"""select job_id from {work_db}.rocky_ingestion_metadata where source_table='{table}'""").collect()[0][0]
+    print(job_id)
+    try:
+      run_info = trigger_rocky_job(json.dumps({"job_id":job_id}))
+      print('response:',run_info)
+      #run_id = json.loads(run_info)['run_id']
+      #print(run_id)
+    except Exception as e:
+      print('failed for',table,e)
+
+
+# COMMAND ----------
+
+
