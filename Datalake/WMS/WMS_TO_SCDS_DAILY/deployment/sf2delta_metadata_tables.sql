5e1d8897471e715eb492cf9690a667c26b9f91d4
pshekhar <pshekhar@petsmart.com>
2023-08-14 14:07:13 -0700

    Update sf2delta_metadata_tables.sql

--- Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/sf2delta_metadata_tables.sql
+++ Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/sf2delta_metadata_tables.sql
@@ -9,7 +9,8 @@ CREATE TABLE raw.historical_run_details_from_sf (
   status STRING,
   error STRING,
   run_date TIMESTAMP)
-USING delta;
+USING delta
+LOCATION "gs://petm-bdpl-prod-raw-p1-gcs-gbl/historical_run_details_from_sf";
 
 
 CREATE TABLE raw.sf2delta_pkey_tstcols (
@@ -17,6 +18,7 @@ CREATE TABLE raw.sf2delta_pkey_tstcols (
   pKeys STRING,
   tstmp_cols STRING,
   tstmp_cols1 STRING)
-USING delta;
+USING delta
+LOCATION "gs://petm-bdpl-prod-raw-p1-gcs-gbl/sf2delta_pkey_tstcols";
 
 insert into raw.sf2delta_pKey_tstcols select * from dev_raw.sf2delta_pkey_tstcols_external;
