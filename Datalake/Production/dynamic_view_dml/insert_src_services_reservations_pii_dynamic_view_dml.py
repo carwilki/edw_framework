a4bf8f3dfa487039545dcb400bb55a4d3ad6f140
rjalan@petsmart.com <rjalan@petsmart.com>
2023-08-24 17:36:42 +0000

    DDT1-8755: added insert statement to migrate pii columns to prod

--- Datalake/Production/dynamic_view_dml/insert_src_services_reservations_pii_dynamic_view_dml.py
+++ Datalake/Production/dynamic_view_dml/insert_src_services_reservations_pii_dynamic_view_dml.py
@@ -18,3 +18,7 @@ VALUES (
 "cust_sensitive", "raw_src_services_reservation_pre", "raw", "src_services_reservation_pre",
 False, "rjalan", current_timestamp(), current_timestamp()
 );
+
+# COMMAND ----------
+
+insert into pii_metadata.pii_metadata_store select * from pii_metadata.pii_metadata_store_qa where table_name = 'raw_src_services_reservation_pre'
