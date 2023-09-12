b42c036c64f973ed2d1806a386f0599e9794756e
pravi <pravi.petsmart.com>
2023-09-12 17:19:04 +0530

    added a deployment script for param file

---
+++ Datalake/BA_Dimension/WF_SITE_HOURS_DAY/deployment/prod_parameter_config_site_hours_day.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+from Datalake.utils.genericUtilities import *
+
+raw='raw'
+parameter_file_name='BA_Dimension_Parameter.prm'
+parameter_section='BA_Dimension.WF:wf_site_hours_day'
+parameter_key='source_url'
+parameter_value='https://petm-qa-facility-svc.cloudhub.io/facility-svc/v1/store/all/hours'  #qa
+#parameter_value='https://petm-prd-facility-svc.cloudhub.io/facility-svc/v1/store/all/hours%22'  #prod
+
+#DO NOT Execute this script twice which will create the duplicates and the workflow will fail
+insert_param_config(raw,parameter_file_name,parameter_section,parameter_key,parameter_value) #Assumption is, this table already exist in Prod
