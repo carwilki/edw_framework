bf2cb16f233c02abbef1e393e8e36ef3eb984811
rsingha <rsingha@petsmart.com>
2023-09-20 19:43:02 +0530

    Duplicates files removed

--- Datalake/Labor_Mgmt/WFA_TIME_SHEET_PUNCH/deployment/prod_DDL_sync_identity_time_sheet_punch.sql
+++
@@ -1,9 +0,0 @@
---*****  Alter Table Sync Identity:  "WFA_TASK" 
-
-alter table qa_legacy.wfa_task alter column WFA_TASK_ID sync identity
-
---*****  Alter Table Sync Identity:  "WFA_DEPARTMENT" 
-alter table qa_legacy.wfa_department alter column WFA_DEPT_ID sync identity 
-
---*****  Alter Table Sync Identity:  "WFA_BUSINESS_AREA" 
-alter table qa_legacy.wfa_business_area alter column wfa_busn_area_id sync identity
