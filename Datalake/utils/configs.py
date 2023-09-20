bf7c9a636c1420dae8fe88db4264e1bc2d89e9be
rsingha <rsingha@petsmart.com>
2023-09-20 19:34:38 +0530

    or_kro_read function added

--- Datalake/utils/configs.py
+++ Datalake/utils/configs.py
@@ -166,6 +166,28 @@ def dc41(env):
 
         return (username, password, connection_string)
 
+
+def or_kro_read(env):
+    if env.lower() == "dev" or env.lower() == "qa":
+        username = "SVC_BD_ORA_NP_READ"
+        hostname = "172.17.89.238"
+        portnumber = "1800"
+        db = "krop1"
+        password = secrets.get(scope="SVC_BD_ORA_NP_READ", key="temp_krop1_password")
+        connection_string = f"jdbc:oracle:thin:@//{hostname}:{portnumber}/{db}.world"
+
+        return (username, password, connection_string)
+
+    if env.lower() == "prod":
+        username = secrets.get(scope="SVC_BD_ORA_P_READ", key="username")
+        password = secrets.get(scope="SVC_BD_ORA_P_READ", key="krop1_password")
+        hostname = "172.20.89.166"
+        portnumber = "1810"
+        db = "krop1"
+        connection_string = f"jdbc:oracle:thin:@//{hostname}:{portnumber}/{db}.world"
+        return (username, password, connection_string)
+
+
 def esdh_prd_sqlServer(env):
     if env.lower() == "dev" or env.lower() == "qa":
         username = secrets.get(scope="svc_bd_sql_np_read", key="mtx_username")
