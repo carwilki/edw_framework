7a79d4cd156d64546afb2ebc8b2f4f95a81225fd
rsingha <rsingha@petsmart.com>
2023-09-21 19:58:00 +0530

    Workforce_Analytics files removed

--- Datalake/Labor_Mgmt/WF_Workforce_Analytics/deployment/2_prod_DDL_wf_Workforce_Analytics_Enable_Change_Feed.sql
+++
@@ -1,8 +0,0 @@
---enables the cdf feature for the following tables
-use legacy;
-ALTER TABLE WFA_FCST_SLS_TASK SET TBLPROPERTIES (delta.enableChangeDataFeed = true);
-ALTER TABLE WFA_FCST_SLS_DEPT SET TBLPROPERTIES (delta.enableChangeDataFeed = true);
-
-use empl_protected;
-ALTER TABLE legacy_WFA_TSCHD SET TBLPROPERTIES (delta.enableChangeDataFeed = true);
-ALTER TABLE legacy_WFA_TDTL SET TBLPROPERTIES (delta.enableChangeDataFeed = true);
