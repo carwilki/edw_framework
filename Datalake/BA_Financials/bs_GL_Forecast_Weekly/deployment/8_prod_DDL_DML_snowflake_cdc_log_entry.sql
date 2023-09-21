00379a0418af3def6e8ded194981f4f717c3e39b
rsingha <rsingha@petsmart.com>
2023-09-21 20:13:51 +0530

    snowflake_cd_log corrected

--- Datalake/BA_Financials/bs_GL_Forecast_Weekly/deployment/8_prod_DDL_DML_snowflake_cdc_log_entry.sql
+++ Datalake/BA_Financials/bs_GL_Forecast_Weekly/deployment/8_prod_DDL_DML_snowflake_cdc_log_entry.sql
@@ -10,6 +10,6 @@ USING delta
 LOCATION 'gs://petm-bdpl-prod-systemdb-p1-gcs-gbl/metadata/tables/snowflake_cdc_log';
 
  
-INSERT INTO legacy.snowflake_cdc_log values("legacy","GL_PLAN_FORECAST_MONTH","EDW_PROD","public","gl_plan_forecast_month_lgcy",NULL,CURRENT_DATE);
+INSERT INTO stranger_things.snowflake_cdc_log values("legacy","GL_PLAN_FORECAST_MONTH","EDW_PROD","public","gl_plan_forecast_month_lgcy",NULL,CURRENT_DATE);
 
-update legacy.snowflake_cdc_log set version = (SELECT max(version) FROM (DESCRIBE HISTORY legacy.GL_PLAN_FORECAST_MONTH)) where dlTable = "GL_PLAN_FORECAST_MONTH";
+update stranger_things.snowflake_cdc_log set version = (SELECT max(version) FROM (DESCRIBE HISTORY legacy.GL_PLAN_FORECAST_MONTH)) where dlTable = "GL_PLAN_FORECAST_MONTH";
