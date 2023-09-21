93bf2fe862e90f87101ca3ac176df0bdd8af83c0
rrajamani <rrajamani@petsmart.com>
2023-09-21 12:44:10 -0700

    CURRENT_TIMESTAMP instead of date

--- Datalake/Labor_Mgmt/WFA_TIME_SHEET_PUNCH/deployment/8_prod_DDL_DML_snowflake_cdc_log_entry.sql.sql
+++ Datalake/Labor_Mgmt/WFA_TIME_SHEET_PUNCH/deployment/8_prod_DDL_DML_snowflake_cdc_log_entry.sql.sql
@@ -12,6 +12,6 @@ LOCATION 'gs://petm-bdpl-prod-systemdb-p1-gcs-gbl/metadata/tables/snowflake_cdc_
 
 -- COMMAND ----------
 
-INSERT INTO stranger_things.snowflake_cdc_log values("legacy","WFA_TIME_SHEET_PUNCH","EDW_PROD","public","wfa_time_sheet_punch_lgcy",NULL,CURRENT_DATE);
+INSERT INTO stranger_things.snowflake_cdc_log values("legacy","WFA_TIME_SHEET_PUNCH","EDW_PROD","public","wfa_time_sheet_punch_lgcy",NULL,CURRENT_TIMESTAMP);
 
 update stranger_things.snowflake_cdc_log set version = (SELECT max(version) FROM (DESCRIBE HISTORY legacy.WFA_TIME_SHEET_PUNCH)) where dlTable = "WFA_TIME_SHEET_PUNCH";
