4c70d2ae9dbbb713ed482a22d67101a833db04d7
rrajamani <rrajamani@petsmart.com>
2023-09-21 09:49:13 -0700

    Remove table ddl snowflake_cdc_log

--- Datalake/BA_Financials/bs_GL_Forecast_Weekly/deployment/8_prod_DDL_DML_snowflake_cdc_log_entry.sql
+++ Datalake/BA_Financials/bs_GL_Forecast_Weekly/deployment/8_prod_DDL_DML_snowflake_cdc_log_entry.sql
@@ -1,13 +1,13 @@
-CREATE TABLE stranger_things.snowflake_cdc_log (
-  dlSchema STRING,
-  dlTable STRING,
-  targetDatabase STRING,
-  targetSchema STRING,
-  targetTable STRING,
-  version BIGINT,
-  timestamp TIMESTAMP)
-USING delta
-LOCATION 'gs://petm-bdpl-prod-systemdb-p1-gcs-gbl/metadata/tables/snowflake_cdc_log';
+-- CREATE TABLE stranger_things.snowflake_cdc_log (
+--   dlSchema STRING,
+--   dlTable STRING,
+--   targetDatabase STRING,
+--   targetSchema STRING,
+--   targetTable STRING,
+--   version BIGINT,
+--   timestamp TIMESTAMP)
+-- USING delta
+-- LOCATION 'gs://petm-bdpl-prod-systemdb-p1-gcs-gbl/metadata/tables/snowflake_cdc_log';
 
  
 INSERT INTO stranger_things.snowflake_cdc_log values("legacy","GL_PLAN_FORECAST_MONTH","EDW_PROD","public","gl_plan_forecast_month_lgcy",NULL,CURRENT_DATE);
