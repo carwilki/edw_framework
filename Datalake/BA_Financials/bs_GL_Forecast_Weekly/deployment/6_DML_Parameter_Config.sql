94899a23eb797ab46a6921f1092dd7ac4b15780c
rsingha <rsingha@petsmart.com>
2023-09-21 21:59:02 +0530

    QA changed to Prod

--- Datalake/BA_Financials/bs_GL_Forecast_Weekly/deployment/6_DML_Parameter_Config.sql
+++ Datalake/BA_Financials/bs_GL_Forecast_Weekly/deployment/6_DML_Parameter_Config.sql
@@ -3,5 +3,5 @@ VALUES('BA_Financials_Parameter.prm','BA_Financials.WF:bs_GL_Forecast_Weekly','s
 
 
 INSERT INTO raw.parameter_config (parameter_file_name,parameter_section,parameter_key,parameter_value)
-VALUES('BA_Financials_Parameter.prm','BA_Financials.WF:bs_GL_Forecast_Weekly','source_bucket','gs://petm-bdpl-qa-raw-p1-gcs-gbl/nas/financials/bpc_plan_forecast/');
+VALUES('BA_Financials_Parameter.prm','BA_Financials.WF:bs_GL_Forecast_Weekly','source_bucket','gs://petm-bdpl-prod-raw-p1-gcs-gbl/nas/financials/bpc_plan_forecast/');
 
