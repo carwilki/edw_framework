dcc257e19994c798b44c2be8ba96b73709f80233
Abhish Ijari <abhish.ijari@databricks.com>
2023-09-28 16:33:04 +0530

    changing the param file

--- Datalake/LaborMgmt/wf_LMS_Holiday_Freight/deployment/5_prod_parameter_config_wf_LMS_Holiday_Freight.sql
+++ Datalake/LaborMgmt/wf_LMS_Holiday_Freight/deployment/5_prod_parameter_config_wf_LMS_Holiday_Freight.sql
@@ -1,6 +1,13 @@
+# Databricks notebook source
+from Datalake.utils.genericUtilities import *
 
-INSERT INTO raw.parameter_config (parameter_file_name,parameter_section,parameter_key,parameter_value) VALUES('INT_Labor_Mgmt_Parameter.prm','INT_Labor_Mgmt.WF:wf_LMS_Holiday_Freight.M:m_ZTB_Holiday_STO_pre','source_bucket','gs://petm-bdpl-prod-raw-p1-gcs-gbl/sap/masterdata/employee/ztb_holiday_sto/');
+raw = "raw"
+parameter_file_name = "INT_Labor_Mgmt_Parameter.prm"
+parameter_section = "INT_Labor_Mgmt.WF:wf_LMS_Holiday_Freight"
+parameter_key = "source_bucket"
+parameter_value = "gs://petm-bdpl-prod-raw-p1-gcs-gbl/sap/masterdata/employee/"
 
-INSERT INTO raw.parameter_config (parameter_file_name,parameter_section,parameter_key,parameter_value) VALUES('INT_Labor_Mgmt_Parameter.prm','INT_Labor_Mgmt.WF:wf_LMS_Holiday_Freight.M:m_ZTB_Event_Hold_pre','source_bucket','gs://petm-bdpl-prod-raw-p1-gcs-gbl/sap/masterdata/employee/ztb_event_hold/');
-
-INSERT INTO raw.parameter_config (parameter_file_name,parameter_section,parameter_key,parameter_value) VALUES('INT_Labor_Mgmt_Parameter.prm','INT_Labor_Mgmt.WF:wf_LMS_Holiday_Freight.M:m_ZTB_Event_CTRL_pre','source_bucket','gs://petm-bdpl-prod-raw-p1-gcs-gbl/sap/masterdata/employee/ztb_event_ctrl/');
+# DO NOT Execute this script twice which will create the duplicates and the workflow will fail
+insert_param_config(
+    raw, parameter_file_name, parameter_section, parameter_key, parameter_value
+)  # Assumption is, this table already exist in Prod
