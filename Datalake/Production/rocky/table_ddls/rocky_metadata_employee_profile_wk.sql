6b878e1d42eeb24b3b9e660cc09094149cf537a3
dbodake@petsmart.com <dbodake@petsmart.com>
2023-10-04 16:56:16 +0000

    DDT1-2615 updating the insert statement

--- Datalake/Production/rocky/table_ddls/rocky_metadata_employee_profile_wk.sql
+++ Datalake/Production/rocky/table_ddls/rocky_metadata_employee_profile_wk.sql
@@ -1,29 +1,5 @@
 -- Databricks notebook source
--- MAGIC %python
--- MAGIC if 'databricksprod' in spark.conf.get('spark.databricks.clusterUsageTags.gcpProjectId'):
--- MAGIC   print(spark.conf.get('spark.databricks.clusterUsageTags.gcpProjectId'))
--- MAGIC   print("Executing Run On Production Cluster...")
--- MAGIC   v_work_db = "work"
--- MAGIC   v_nz_db = "NZ_Mako8_legacy"
--- MAGIC   v_legacy_db = "legacy"
--- MAGIC else:
--- MAGIC   print(spark.conf.get('spark.databricks.clusterUsageTags.gcpProjectId'))
--- MAGIC   print("Executing Run On Dev/QA Cluster...")
--- MAGIC   v_work_db = "qa_work"
--- MAGIC   v_nz_db = "NZ_Mako4_legacy"
--- MAGIC   v_legacy_db = "qa_legacy"
--- MAGIC
--- MAGIC dbutils.widgets.text("work_db", v_work_db)
--- MAGIC dbutils.widgets.text("nz_db", v_nz_db)
--- MAGIC dbutils.widgets.text("legacy_db", v_legacy_db)
-
--- COMMAND ----------
-
-drop table if exists ${legacy_db}.employee_profile_wk
-
--- COMMAND ----------
-
-INSERT INTO ${work_db}.rocky_ingestion_metadata (
+INSERT INTO work.rocky_ingestion_metadata (
 table_group, table_group_desc, source_type, source_db, source_table, 
 table_desc, is_pii, pii_type, has_hard_deletes, target_sink, 
 target_db, target_schema, target_table_name, load_type, source_delta_column,
@@ -32,7 +8,7 @@ expected_start_time, job_watchers, max_retry, job_tag, is_scheduled,
 job_id, snowflake_ddl , tidal_trigger_condition , disable_no_record_failure , snowflake_pre_sql , snowflake_post_sql ,additional_config
 )
 VALUES (
-"NZ_Migration", null, '${nz_db}', "EDW_PRD", "EMPLOYEE_PROFILE_WK",
+"NZ_Migration", null, 'NZ_Mako8_legacy', "EDW_PRD", "EMPLOYEE_PROFILE_WK",
 null, true, "employee_protected", false, "delta",
 "legacy", null, "employee_profile_wk", "full", null, 
 null, null, "daily", null, array("DUMMY_TIDAL_JOB"), 
