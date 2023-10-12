5184730d570719d8f583376d912ba170b4f429b4
Carson Wilkins <cwilkins01@petsmart.com>
2023-10-12 09:42:04 -0700

    directory changes

---
+++ Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/install/update_pallet_smoothing.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+%sql
+UPDATE <remove_me_>refine.wm_task_hdr set 
+    WM_TASK_BATCH = 'Pallet Smoothing',
+    UPDATE_TSTMP = current_timestamp()
+WHERE 
+    WM_TASK_BATCH like '%Pallet%';
