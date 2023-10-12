a3ca3d6abe0e59e80ae527b5a9ed454e8681957a
Carson Wilkins <cwilkins01@petsmart.com>
2023-10-12 09:37:01 -0700

    directory changes for a clear install path

--- Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/deploy_jobs.sh
+++
@@ -1,3 +0,0 @@
-databricks jobs create --version 2.1 --json-file Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/create_wf_WMS_TO_SCDS_DAILY_A_I.json 
-databricks jobs create --version 2.1 --json-file Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/create_wf_WMS_TO_SCDS_DAILY_I_P.json 
-databricks jobs create --version 2.1 --json-file Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/create_wf_WMS_TO_SCDS_DAILY_P_Y.json 
--- Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/update_pallet_smoothing.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-%sql
-UPDATE <remove_me_>refine.wm_task_hdr set 
-    WM_TASK_BATCH = 'Pallet Smoothing',
-    UPDATE_TSTMP = current_timestamp()
-WHERE 
-    WM_TASK_BATCH like '%Pallet%';
