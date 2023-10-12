a078d074c5f8b16c0f21363c9b4b3ae41eda7443
pravi <pravi.petsmart.com>
2023-10-12 19:28:06 +0530

    Script changes for DNM 5983

--- Datalake/BA_Financials/bs_GL_Forecast_Weekly/deployment/8_prod_DDL_DML_snowflake_cdc_log_entry.sql
+++ Datalake/BA_Financials/bs_GL_Forecast_Weekly/deployment/8_prod_DDL_DML_snowflake_cdc_log_entry.sql
@@ -10,6 +10,6 @@
 -- LOCATION 'gs://petm-bdpl-prod-systemdb-p1-gcs-gbl/metadata/tables/snowflake_cdc_log';
 
  
-INSERT INTO stranger_things.snowflake_cdc_log values("legacy","GL_PLAN_FORECAST_MONTH","EDW_PROD","public","gl_plan_forecast_month_lgcy",NULL,CURRENT_TIMESTAMP);
+--INSERT INTO stranger_things.snowflake_cdc_log values("legacy","GL_PLAN_FORECAST_MONTH","EDW_PROD","public","gl_plan_forecast_month_lgcy",NULL,CURRENT_TIMESTAMP);
 
 update stranger_things.snowflake_cdc_log set version = (SELECT max(version) FROM (DESCRIBE HISTORY legacy.GL_PLAN_FORECAST_MONTH)) where dlTable = "GL_PLAN_FORECAST_MONTH";
