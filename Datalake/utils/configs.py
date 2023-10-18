b82a601093cf936f603435fa9171d9c6a4013b0e
rsingha <rsingha@petsmart.com>
2023-10-18 19:17:40 +0530

    added evdh_prd_sqlServer function in utils

--- Datalake/utils/configs.py
+++ Datalake/utils/configs.py
@@ -551,3 +551,28 @@ def or_kro_read_edhp1(env):
         return (username, password, connection_string)
 
     raise Exception(f"Environment {env} is not supported")
+
+
+def evdh_prd_sqlServer(env):
+    if env.lower() == "dev" or env.lower() == "qa":
+        username = secrets.get(scope="svc_bd_sql_np_read", key="mtx_username")
+        password = secrets.get(scope="svc_bd_sql_np_read", key="mtx_password")
+        hostname = "172.17.89.188"
+        portnumber = "1840"
+        db = "MTX_PRD"
+        connection_string = f"""jdbc:sqlserver://{hostname}:{portnumber};databaseName={db};encrypt=true;trustServerCertificate=true;"""
+
+        return (username, password, connection_string)
+
+    if env.lower() == "prod":
+        # username, password, hostname
+        username = secrets.get(scope="SVC_BD_SQL_READ", key="username")
+        password = secrets.get(scope="SVC_BD_SQL_READ", key="esdh_password")
+        hostname = "172.20.89.138"
+        portnumber = "1840"
+        db = "EnterpriseVendorDataHub"
+        connection_string = f"""jdbc:sqlserver://{hostname}:{portnumber};databaseName={db};encrypt=true;trustServerCertificate=true;"""
+
+        return (username, password, connection_string)
+    raise Exception(f"Environment {env} is not supported")
+
