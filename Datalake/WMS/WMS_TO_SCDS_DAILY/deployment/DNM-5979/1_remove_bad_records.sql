36b5883145b3bfa4bf6544b39d034806b6cd61a6
Carson Wilkins <cwilkins01@petsmart.com>
2023-10-23 11:17:26 -0700

    updated envs

--- Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/DNM-5979/1_remove_bad_records.sql
+++ Datalake/WMS/WMS_TO_SCDS_DAILY/deployment/DNM-5979/1_remove_bad_records.sql
@@ -3,3 +3,5 @@ where exists (
 select LOCATION_ID,WM_C_TMS_PLAN_ID from
 qa_work.raptor_dataset_src_extra_bd_vs_sf_NZ_UAT_WM_C_TMS_PLAN_10012023_190923 
 where a.location_id = location_id and a.wm_c_tms_plan_id = WM_C_TMS_PLAN_ID);
+
+optimize refine.wm_c_tms_plan
