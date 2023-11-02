993989a3dfb87083c74b0ca121f7f57b9a601ab6
Archana H <ah@petsmart.com>
2023-11-02 18:47:07 +0530

    Adding deployment scripts

---
+++ Datalake/BA_Inventory/bs_Inventory_Day/deployment/4_prod_rocky_hist_to_legacy_bs_Inventory_Day.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+from pyspark.sql.functions import col
+import csv
+
+tables = ["INV_SKU_STORE","INV_INSTOCK_PRICE_DAY","INV_HOLES_COUNT"]
+
+
+for table in tables:
+    print(table)
+    rocky_table = f"refine.{table}_history"
+    target_table = f"legacy.{table}"
+    df = spark.sql(f"select * from {rocky_table}")
+    df = df.drop(
+        col("bd_create_dt_tm"), col("bd_update_dt_tm"), col("source_file_name")
+    )
+    df.write.insertInto(f"{target_table}", overwrite=True)
