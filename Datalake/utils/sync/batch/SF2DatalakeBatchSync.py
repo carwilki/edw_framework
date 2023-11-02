7880c80f7dbbf35260ad466aa0e9248157f3f66a
Carson Wilkins <cwilkins01@petsmart.com>
2023-11-02 11:38:41 -0700

    change args to script

--- Datalake/utils/sync/batch/SF2DatalakeBatchSync.py
+++ Datalake/utils/sync/batch/SF2DatalakeBatchSync.py
@@ -45,7 +45,8 @@ parser.add_argument(
     default=None,
 )
 parser.add_argument(
-    "-dc" "--date_columns",
+    "-dc",
+    "--date_columns",
     type=str,
     help="colunms that are dates to be used as the iterator to get new data",
     required=True,
@@ -53,12 +54,16 @@ parser.add_argument(
 parser.add_argument(
     "-sd",
     "--start_dt",
-    type=datetime,
+    type=lambda x: datetime.strptime(x, "%Y-%m-%d"),
     help="the start date for the batch",
     required=True,
 )
 parser.add_argument(
-    "-ed", "--end_dt", type=datetime, help="the end date for the batch", required=True
+    "-ed",
+    "--end_dt",
+    type=lambda x: datetime.strptime(x, "%Y-%m-%d"),
+    help="the end date for the batch",
+    required=True,
 )
 parser.add_argument(
     "-in",
@@ -79,8 +84,8 @@ batchConfig.source_filter = args.source_filter
 batchConfig.keys = [key for key in args.keys.split(",")]
 batchConfig.excluded_columns = [col for col in args.excluded_columns.split(",")]
 batchConfig.date_columns = [date for date in args.date_columns.split(",")]
-batchConfig.start_dt = args.start_dt.strftime("%Y-%m-%")
-batchConfig.end_dt = args.end_dt.strftime("%Y-%m-%")
+batchConfig.start_dt = args.start_dt
+batchConfig.end_dt = args
 batchConfig.interval = args.interval
 batchConfig.current_dt = None
 
