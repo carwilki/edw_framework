cb5bda372c2caa92a4f5a63086cccf536b6aa318
Carson Wilkins <cwilkins01@petsmart.com>
2023-11-06 12:14:17 -0700

    had to rework the writer

--- Datalake/utils/sync/writer/SparkDeltaLakeBatchWriter.py
+++ Datalake/utils/sync/writer/SparkDeltaLakeBatchWriter.py
@@ -20,19 +20,37 @@ class SparkDeltaLakeBatchWriter(AbstractBatchWriter):
         print(f"SparkDeltaLakeBatchWriter::_build_merge_key::key={key}")
         return key
 
-    def write(self, source: DataFrame) -> None:
+    def _create_and_write(self, source: DataFrame) -> None:
+        if not self.spark.catalog.tableExists(self.config.target_table_fqn):
+            print(
+                f"SparkDeltaLakeBatchWriter::_create_and_write::creating {self.config.target_table_fqn}"
+            )
+
+            source.write.format("delta").mode("overwrite").save(
+                self.config.target_table_fqn
+            )
+
+    def _upsert(self, source: DataFrame) -> None:
         print(
-            f"SparkDeltaLakeBatchWriter::write::merging to {self.config.target_table_fqn}"
+            f"SparkDeltaLakeBatchWriter::_upsert::merging to {self.config.target_table_fqn}"
         )
+
         target: DeltaTable = DeltaTable.forName(
             self.spark, self.config.target_table_fqn
         )
         keys = self._build_merge_key("source", "target")
-        target.merge(
-            source, keys
+        target.alias("target").merge(
+            source.alias("source"), keys
         ).whenMatchedUpdateAll().whenNotMatchedInsertAll().execute()
         print(
-            f"SparkDeltaLakeBatchWriter::write::compacting {self.config.target_table_fqn}"
+            f"SparkDeltaLakeBatchWriter::_upsert::compacting {self.config.target_table_fqn}"
         )
         target.optimize().executeCompaction()
-        print("SparkDeltaLakeBatchWriter::write::done")
+        print("SparkDeltaLakeBatchWriter::_upsert::done")
+
+    def write(self, df: DataFrame) -> None:
+        if self.spark.catalog.tableExists(self.config.target_table_fqn):
+            self._upsert(df)
+        else:
+            print("SparkDeltaLakeBatchWriter::write::target table not found...creating")
+            self._create_and_write(df)
