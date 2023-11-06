2bd670cdd54c1ae078199804bc1f0da8f914db84
Carson Wilkins <cwilkins01@petsmart.com>
2023-11-06 15:33:40 -0700

    netezza reader implementation ready for test.

--- Datalake/utils/sync/nz_vars.py
+++ Datalake/utils/sync/nz_vars.py
@@ -5,3 +5,4 @@ nz_jdbc_num_part = 9
 nz_jdbc_url = "jdbc:netezza://172.16.73.181:5480/"
 nz_username = secrets.get(scope="netezza_petsmart_keys", key="username")
 nz_password = secrets.get(scope="netezza_petsmart_keys", key="password")
+nz_fetch_size = 100_000
--- Datalake/utils/sync/reader/NetezzaBatchReader.py
+++ Datalake/utils/sync/reader/NetezzaBatchReader.py
@@ -1,12 +1,12 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from pyspark.sql import DataFrame, SparkSession
 
-from Datalake.utils import secrets
 from Datalake.utils.genericUtilities import getSFEnvSuffix
 from Datalake.utils.sync.batch.BatchReaderSourceType import BatchReaderSourceType
 from Datalake.utils.sync.batch.DateRangeBatchConfig import DateRangeBatchConfig
 from Datalake.utils.sync.reader.AbstractBatchReader import AbstractBatchReader
+from Datalake.utils.sync import nz_vars
 
 
 class NetezzaJDBCBatchReader(AbstractBatchReader):
@@ -37,37 +37,27 @@ class NetezzaJDBCBatchReader(AbstractBatchReader):
         self.config.source_table_fqn = (
             f"{self.nz_database}.{self.nz_schema}.{self.nz_table}"
         )
-        print(f"NetezzaJDBCBatchReader::_setup_reader::sf_database: {self.nz_database}")
-        print(f"NetezzaJDBCBatchReader::_setup_reader::sf_schema: {self.nz_schema}")
-        print(f"NetezzaJDBCBatchReader::_setup_reader::sf_table {self.nz_table}")
+        print(f"NetezzaJDBCBatchReader::_setup_reader::nz_database: {self.nz_database}")
+        print(f"NetezzaJDBCBatchReader::_setup_reader::nz_schema: {self.nz_schema}")
+        print(f"NetezzaJDBCBatchReader::_setup_reader::nz_table {self.nz_table}")
 
         print(f"NetezzaJDBCBatchReader::_setup_reader::env: {self.env}")
-        if self.env == "prod":
-            self.nzOptions = {
-                "driver": config.get("netezza_jdbc_driver"),
-                "url": f"""{config.get("netezza_jdbc_url")}{self.config.source_schema};""",
-                "fetchsize": 100000,
-                "user": config.get("netezza_jdbc_user"),
-                "password": config.get("netezza_jdbc_password"),
-                "numPartitions": config.get("netezza_jdbc_num_part"),
-            }
-        else:
-            self.nzOptions = {
-                "driver": config.get("netezza_jdbc_driver"),
-                "url": f"""{config.get("netezza_jdbc_url")}{self.config.source_schema};""",
-                "fetchsize": 100000,
-                "user": config.get("netezza_jdbc_user"),
-                "password": config.get("netezza_jdbc_password"),
-                "numPartitions": config.get("netezza_jdbc_num_part"),
-            }
+        self.nzOptions: dict = {
+            "driver": nz_vars.nz_jdbc_driver,
+            "url": f"""{nz_vars.nz_jdbc_url}{self.nz_schema};""",
+            "fetchsize": nz_vars.nz_fetch_size,
+            "user": nz_vars.nz_username,
+            "password": nz_vars.nz_password,
+            "numPartitions": nz_vars.nz_jdbc_num_part,
+        }
 
-        print(f"NetezzaJDBCBatchReader::_setup_reader::sfOptions: {self.sfOptions}")
+        print(f"NetezzaJDBCBatchReader::_setup_reader::sfOptions: {self.nzOptions}")
 
     def _validate_sf_config(self, config: DateRangeBatchConfig):
         print("NetezzaJDBCBatchReader::_validate_sf_config::validating sf config")
-        if config.source_type != BatchReaderSourceType.SNOWFLAKE:
+        if config.source_type != BatchReaderSourceType.NETEZZA:
             raise ValueError(
-                "source_type must be set to Snowflake for use with the NetezzaJDBCBatchReader"
+                "source_type must be set to Neteeza for use with the NetezzaJDBCBatchReader"
             )
         if config.source_table_fqn is None:
             raise ValueError(
@@ -76,7 +66,7 @@ class NetezzaJDBCBatchReader(AbstractBatchReader):
 
     def _generate_query(self, dt: datetime) -> str:
         print("NetezzaJDBCBatchReader::_generate_query::generating query")
-        query = f"""select * from {self.nz_table}"""
+        query = f"""select * from {self.config.source_table_fqn}"""
         where = ""
         s_dt = dt.strftime("%Y-%m-%d 00:00:00")
         e_dt = (dt + self.config.interval).strftime("%Y-%m-%d 00:00:00")
@@ -94,9 +84,6 @@ class NetezzaJDBCBatchReader(AbstractBatchReader):
         )
         return query
 
-    def _execute_query(self, query: str) -> DataFrame:
-        self.spark.read()
-
     def _strip_colunms(self, df: DataFrame) -> DataFrame:
         print("NetezzaJDBCBatchReader::_strip_colunms::stripping excluded columns")
         df = df.drop(*self.config.excluded_columns)
@@ -108,28 +95,9 @@ class NetezzaJDBCBatchReader(AbstractBatchReader):
             table:      {self.config.source_table_fqn}
             on range:   {self.config.current_dt} to {self.config.current_dt + self.config.interval}"""
         )
-        
+        self.nzOptions[
+            "dbtable"
+        ] = f"({self._generate_query(self.config.current_dt)}) as data"
+        df = self.spark.read.format("jdbc").options(**self.nzOptions).load()
         df = self._convert_decimal_to_int_types(df)
         df = self._strip_colunms(df)
-
-    def read(self) -> DataFrame:
-        """
-        Reads data from a JDBC source table and returns it as a DataFrame.
-
-        Returns:
-            DataFrame: A DataFrame containing the data from the JDBC source table.
-        """
-        config = HarnessJobManagerEnvironment.getConfig()
-        SQL = (
-            f"""Select * from {self.config.source_schema}.{self.config.source_table}"""
-        )
-
-        if self.config.source_filter is not None:
-            SQL = SQL + f""" WHERE {self.config.source_filter}"""
-
-        SQL = f"""({SQL}) as data"""
-
-        df = self.session.read.format("jdbc").options(**reader_options).load()
-
-
-        return df
