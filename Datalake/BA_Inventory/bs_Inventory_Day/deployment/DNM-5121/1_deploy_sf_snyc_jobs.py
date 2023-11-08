a1df043a1c9c0d1ccbf5b627812691a381491510
Carson Wilkins <cwilkins01@petsmart.com>
2023-11-08 14:52:13 -0700

    added deploy for DNM-5121 and refactored some job installations.

---
+++ Datalake/BA_Inventory/bs_Inventory_Day/deployment/DNM-5121/1_deploy_sf_snyc_jobs.py
@@ -0,0 +1,83 @@
+# Databricks notebook source
+import requests
+import json
+
+from Datalake.utils import secrets
+
+token = secrets.get(scope="db-token-jobsapi", key="password")
+google_service_account = (
+    "petm-bdpl-bricksengprd-p-sa@petm-prj-bricksengprd-p-2f96.iam.gserviceaccount.com"
+)
+instance_id = secrets.get(scope="db-token-jobsapi", key="instance_id")
+
+# COMMAND ----------
+
+
+def create_job(payload):
+    api_version = "/api/2.1"
+    api_command = "/jobs/create"
+    url = f"https://{instance_id}{api_version}{api_command}"
+
+    params = {"Authorization": "Bearer " + token, "Content-Type": "application/json"}
+
+    response = requests.post(url=url, headers=params, data=payload)
+
+    return response.text
+
+
+def getJobId(json_response):
+    import re
+
+    id = json_response.split(":")[1]
+    return re.findall(r"\d+", id)[0]
+
+
+# COMMAND ----------
+
+
+def set_permission(payload, job_id):
+    api_version = "/api/2.0"
+    api_command = f"/permissions/jobs/{job_id}"
+    url = f"https://{instance_id}{api_version}{api_command}"
+
+    params = {"Authorization": "Bearer " + token, "Content-Type": "application/json"}
+
+    response = requests.patch(url=url, headers=params, data=payload)
+
+    return response.text
+
+
+# COMMAND ----------
+
+permission_json = {
+    "access_control_list": [
+        {
+            "group_name": "App_Databricks_DE_Prod_BigDataOperations",
+            "permission_level": "CAN_MANAGE",
+        },
+        {"group_name": "App_Databricks_DE_Prod_Viewer", "permission_level": "CAN_VIEW"},
+    ]
+}
+
+
+# COMMAND ----------
+
+job_json = [
+    "load_dp_accuracy_day_nz.json",
+    "load_dp_forecast_week_hist_nz.json",
+    "load_dp_order_projection_day_hist_nz.json",
+    "load_dp_order_projection_week_hist_nz.json",
+    "load_dp_product_location_settings_hist_nz.json",
+]
+for job_file in job_json:
+    with open(job_file) as json_file:
+        job_payload = json.load(json_file)
+
+        payload = json.dumps(job_payload)
+
+        response = create_job(payload)
+        job_id = getJobId(response)
+        print(response)
+        payload = json.dumps(permission_json)
+        response = set_permission(payload, job_id)
+        print(response)
