6641aad2e8662425c032d95bdea3310d50c279e9
rrajamani <rrajamani@petsmart.com>
2023-11-14 12:26:15 -0800

    Create instock_price_day_log_details_entry.py

---
+++ Datalake/BA_Inventory/bs_Inventory_Day/deployment/install/fix1/instock_price_day_log_details_entry.py
@@ -0,0 +1,6 @@
+-- Databricks notebook source
+-- COMMAND ----------
+from Datalake.utils.genericUtilities import *
+from Datalake.utils.logger import *
+-- COMMAND ----------
+logPrevRunDt("INV_INSTOCK_PRICE_DAY", "INV_INSTOCK_PRICE_DAY", "Completed", "N/A", f"{raw}.log_run_details")
