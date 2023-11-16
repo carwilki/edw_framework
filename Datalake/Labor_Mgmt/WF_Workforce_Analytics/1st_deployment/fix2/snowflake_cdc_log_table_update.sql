d3b91b3db6a74f4ebbc6edfc5080bb1b3c7937d8
rsingha <rsingha@petsmart.com>
2023-11-16 23:03:01 +0530

    Update statements added

--- Datalake/Labor_Mgmt/WF_Workforce_Analytics/1st_deployment/fix2/snowflake_cdc_log_table_update.sql
+++ Datalake/Labor_Mgmt/WF_Workforce_Analytics/1st_deployment/fix2/snowflake_cdc_log_table_update.sql
@@ -1,7 +1,2 @@
-INSERT INTO stranger_things.snowflake_cdc_log 
-select "empl_protected","legacy_WFA_TDTL","EDW_PRD","public","wfa_tdtl_lgcy",version,CURRENT_TIMESTAMP 
-from (SELECT max(version) as version FROM (DESCRIBE HISTORY empl_protected.legacy_WFA_TDTL));
-
-INSERT INTO stranger_things.snowflake_cdc_log 
-select "empl_protected","legacy_WFA_TSCHD","EDW_PRD","public","wfa_tschd_lgcy",version,CURRENT_TIMESTAMP 
-from (SELECT max(version) as version FROM (DESCRIBE HISTORY empl_protected.legacy_WFA_TSCHD));
+UPDATE stranger_things.snowflake_cdc_log  set dlSchema = "empl_protected",dlTable ="legacy_WFA_TSCHD" where dlTable ="WFA_TSCHD";
+UPDATE stranger_things.snowflake_cdc_log  set dlSchema = "empl_protected",dlTable ="legacy_WFA_TDTL" where dlTable ="WFA_TDTL";
