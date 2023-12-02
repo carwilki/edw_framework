c4b7c524bb41dc8fd3891880866c0bf4feb8db98
rrajamani@petsmart.com <rrajamani@petsmart.com>
2023-12-02 00:44:47 +0000

    use rocky automation scripts

--- Datalake/demand_planning/bs_Demand_Planning_Day/deployment/install/4_bs_Demand_Planning_Day_rocky_trigger.py
+++ Datalake/demand_planning/bs_Demand_Planning_Day/deployment/install/4_bs_Demand_Planning_Day_rocky_trigger.py
@@ -1,7 +1,16 @@
-
+# Databricks notebook source
 from Datalake.utils.rocky_script_automation.RockyAutomationUtils.py import *
 from pyspark.sql.functions import *
 
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
 tables = ['SAP_SKU_LINK_TYPE']
+
+# COMMAND ----------
+
 trigger_rocky(tables)
 
