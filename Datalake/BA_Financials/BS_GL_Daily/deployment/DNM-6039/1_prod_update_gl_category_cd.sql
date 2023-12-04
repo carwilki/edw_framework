a31d5e28e15fc337ec6e0e0a34046d4b06f6f105
rrajamani@petsmart.com <rrajamani@petsmart.com>
2023-12-04 20:02:34 +0000

    update script

--- Datalake/BA_Financials/BS_GL_Daily/deployment/DNM-6039/1_prod_update_gl_category_cd.sql
+++ Datalake/BA_Financials/BS_GL_Daily/deployment/DNM-6039/1_prod_update_gl_category_cd.sql
@@ -1,5 +1,19 @@
+-- Databricks notebook source
 select count(*)  from legacy.GL_ACTUAL_DAY_DETAIL
 where len(GL_CATEGORY_CD)=1
 
+-- COMMAND ----------
+
 update legacy.GL_ACTUAL_DAY_DETAIL
 set GL_CATEGORY_CD= lpad(GL_CATEGORY_CD, 2, '0')
+
+
+-- COMMAND ----------
+
+select count(*)  from legacy.GL_ACTUAL_DAY_DETAIL
+where len(GL_CATEGORY_CD)=1
+
+-- COMMAND ----------
+
+select *  from legacy.GL_ACTUAL_DAY_DETAIL
+where len(GL_CATEGORY_CD)=1
