129be26d870b025fe7e545e246414e759f58672c
abelsare@petsmart.com <abelsare@petsmart.com>
2023-12-08 09:28:11 +0000

    added metadata for notebooks of wf_BPC source tables

--- Datalake/Production/adhoc/create_view_location_area.sql
+++ Datalake/Production/adhoc/create_view_location_area.sql
@@ -1,4 +1,9 @@
 -- Databricks notebook source
+-- MAGIC %md
+-- MAGIC Creating view legacy.location_area on top of refine.sap_location_area as Location_Area is required for wf_BPC
+
+-- COMMAND ----------
+
 -- MAGIC %sql
 -- MAGIC CREATE OR REPLACE VIEW legacy.location_area AS
 -- MAGIC SELECT LOCATION_ID,AREA_ID,CAST(LOC_AREA_EFF_DT AS DATE) LOC_AREA_EFF_DT,CAST(LOC_AREA_END_DT AS DATE) LOC_AREA_END_DT,SQ_FT_AMT FROM refine.sap_location_area
