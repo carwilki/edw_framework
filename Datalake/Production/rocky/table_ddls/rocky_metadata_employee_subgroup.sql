cd65f79c650dcd10de70479a298933cbb9238be5
pkulkarni <pkulkarni>
2023-12-12 17:57:43 +0000

    DDT1-2748 initial commit for rocky metadata entries for the table employee_subgroup

---
+++ Datalake/Production/rocky/table_ddls/rocky_metadata_employee_subgroup.sql
@@ -0,0 +1,22 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ####rocky table employee_subgroup
+
+-- COMMAND ----------
+
+INSERT INTO work.rocky_ingestion_metadata (
+table_group, table_group_desc, source_type, source_db, source_table, 
+table_desc, is_pii, pii_type, has_hard_deletes, target_sink, 
+target_db, target_schema, target_table_name, load_type, source_delta_column,
+primary_key, initial_load_filter, load_frequency, load_cron_expr, tidal_dependencies,
+expected_start_time, job_watchers, max_retry, job_tag, is_scheduled,
+job_id, snowflake_ddl , tidal_trigger_condition , disable_no_record_failure , snowflake_pre_sql , snowflake_post_sql ,additional_config
+)
+VALUES (
+"NZ_Migration", null, 'NZ_Mako8', "EDW_PRD", "employee_subgroup",
+null, false, null, false, "delta",
+"legacy", null, "employee_subgroup", "upsert", "UPDATE_TSTMP", 
+"EMPL_GROUP_CD,EMPL_SUBGROUP_CD", null, "daily", null, array("DUMMY_TIDAL_JOB"), 
+null, array("pkulkarni@petsmart.com","sjaiswal@petsmart.com"), 3, '{"Department":"Netezza-Migration"}', false,
+null, null ,"ALL_MUST_BE_MET" ,true ,null , null, null
+);
