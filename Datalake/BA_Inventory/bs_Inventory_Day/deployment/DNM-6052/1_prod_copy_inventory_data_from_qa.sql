4960eb527fa933a665561e152c01133086f67ea6
rrajamani <rrajamani@petsmart.com>
2023-12-14 10:14:44 -0800

    Update 1_prod_copy_inventory_data_from_qa.sql

--- Datalake/BA_Inventory/bs_Inventory_Day/deployment/DNM-6052/1_prod_copy_inventory_data_from_qa.sql
+++ Datalake/BA_Inventory/bs_Inventory_Day/deployment/DNM-6052/1_prod_copy_inventory_data_from_qa.sql
@@ -6,11 +6,3 @@ TRUNCATE TABLE legacy.INV_HOLES_COUNT
 INSERT INTO legacy.INV_HOLES_COUNT 
 select * except(bd_create_dt_tm, bd_update_dt_tm, source_file_name) from qa_refine.INV_HOLES_COUNT_history
 
--- COMMAND ----------
-
-TRUNCATE TABLE legacy.inv_instock_price_day
-
--- COMMAND ----------
-
-INSERT INTO legacy.inv_instock_price_day
-select * except(bd_create_dt_tm, bd_update_dt_tm, source_file_name) from qa_refine.inv_instock_price_day_history
