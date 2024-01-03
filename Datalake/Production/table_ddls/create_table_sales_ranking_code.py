5721dac3aeaaf1c2f37e1be7e46f27d3799b9263
abelsare@petsmart.com <abelsare@petsmart.com>
2024-01-03 09:46:42 +0000

    DDT1-2768 added markdowns for related notebooks

--- Datalake/Production/table_ddls/create_table_sales_ranking_code.py
+++ Datalake/Production/table_ddls/create_table_sales_ranking_code.py
@@ -1,15 +1,21 @@
 # Databricks notebook source
-CREATE TABLE legacy.sales_ranking_code
-(  
-RANKING_CD STRING NOT NULL, 
-RANKING_START_DT DATE NOT NULL, 
-RANKING_END_DT DATE, 
-RANKING_LEVEL SMALLINT,  
-RANKING_DESC STRING, 
-RANKING_START_PERCENT DECIMAL(7,4),  
-RANKING_END_PERCENT DECIMAL(7,4), 
-LOAD_TSTMP TIMESTAMP
-)
-USING delta LOCATION 'gs://petm-bdpl-prod-nzlegacy-p1-gcs-gbl/BA_Dimension/sales_ranking_code' 
+# MAGIC %md
+# MAGIC ####DDL for sales_ranking_code, which is required for wf_sales_ranking_wk workflow
 
- 
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TABLE legacy.sales_ranking_code
+# MAGIC (  
+# MAGIC RANKING_CD STRING NOT NULL, 
+# MAGIC RANKING_START_DT DATE NOT NULL, 
+# MAGIC RANKING_END_DT DATE, 
+# MAGIC RANKING_LEVEL SMALLINT,  
+# MAGIC RANKING_DESC STRING, 
+# MAGIC RANKING_START_PERCENT DECIMAL(7,4),  
+# MAGIC RANKING_END_PERCENT DECIMAL(7,4), 
+# MAGIC LOAD_TSTMP TIMESTAMP
+# MAGIC )
+# MAGIC USING delta LOCATION 'gs://petm-bdpl-prod-nzlegacy-p1-gcs-gbl/BA_Dimension/sales_ranking_code' 
+# MAGIC
+# MAGIC  
