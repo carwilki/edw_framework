24aca42bbdece6dfac5ce06ee63258e009800b7b
abelsare@petsmart.com <abelsare@petsmart.com>
2024-01-03 07:46:16 +0000

    DDT1-2768 - added rocky metadata scripts of sales_day_sku_store_rpt and usr_store_divisions and ddl script of sales_ranking_code for wf_sales_ranking_wk source tables

---
+++ Datalake/Production/table_ddls/create_table_sales_ranking_code.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+CREATE TABLE legacy.sales_ranking_code
+(  
+RANKING_CD STRING NOT NULL, 
+RANKING_START_DT DATE NOT NULL, 
+RANKING_END_DT DATE, 
+RANKING_LEVEL SMALLINT,  
+RANKING_DESC STRING, 
+RANKING_START_PERCENT DECIMAL(7,4),  
+RANKING_END_PERCENT DECIMAL(7,4), 
+LOAD_TSTMP TIMESTAMP
+)
+USING delta LOCATION 'gs://petm-bdpl-prod-nzlegacy-p1-gcs-gbl/BA_Dimension/sales_ranking_code' 
+
+ 
