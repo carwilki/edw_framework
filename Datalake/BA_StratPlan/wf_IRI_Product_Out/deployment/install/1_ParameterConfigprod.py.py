e9c36028ebdf5583c976eb843c043b9e5eb633ad
asharma05 <asharma05@petsmart.com>
2024-01-05 15:53:16 -0500

    Update 1_ParameterConfigprod.py.py

--- Datalake/BA_StratPlan/wf_IRI_Product_Out/deployment/install/1_ParameterConfigprod.py.py
+++ Datalake/BA_StratPlan/wf_IRI_Product_Out/deployment/install/1_ParameterConfigprod.py.py
@@ -19,10 +19,10 @@ parameter_file_name='wf_IRI_Product_Out'
 parameter_section='m_IRI_Product_Out' 
 parameter_key='Last_Run_date' 
 parameter_value='1900-01-01' 
-insert_param_config(raw,parameter_file_name,parameter_section,parameter_key,parameter_value)
+update_param_config(raw,parameter_file_name,parameter_section,parameter_key,parameter_value)
 
 parameter_file_name='wf_IRI_Product_Out' 
 parameter_section='m_IRI_Product_Out' 
 parameter_key='SKU_Date_Filter' 
-parameter_value='SKU_PROFILE_RPT.UPDATE_DT>to_date("01/01/1900")'
-insert_param_config(raw,parameter_file_name,parameter_section,parameter_key,parameter_value)
+parameter_value="SKU_PROFILE_RPT.UPDATE_DT>to_date('01/01/1900')"
+update_param_config(raw,parameter_file_name,parameter_section,parameter_key,parameter_value)
