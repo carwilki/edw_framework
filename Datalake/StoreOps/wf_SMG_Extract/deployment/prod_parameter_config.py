ab1848493bf1918fccd0a97f54c09a7a67541fc5
rahul-singha-databricks <rahul.singha@databricks.com>
2024-01-09 13:39:49 +0530

    nas patha dded

--- Datalake/StoreOps/wf_SMG_Extract/deployment/prod_parameter_config.py
+++ Datalake/StoreOps/wf_SMG_Extract/deployment/prod_parameter_config.py
@@ -20,4 +20,8 @@ parameter_key='p_sku_dept'
 parameter_value='SKU_PROFILE.SAP_DEPT_ID IN (3136,81)' 
 insert_param_config(raw,parameter_file_name,parameter_section,parameter_key,parameter_value)
 
-
+parameter_file_name='wf_SMG_Extract' 
+parameter_section='m_smg_site_extract' 
+parameter_key='nas_target' 
+parameter_value='petm-bdpl-prod-apps-p1-gcs-gbl/nas_outbound/SMG/smg_site_extract/' 
+insert_param_config(raw,parameter_file_name,parameter_section,parameter_key,parameter_value)
