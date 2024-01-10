54bb12a42d739d38b136e6305a3b6e1119c18a42
carwilki <carwilki@gmail.com>
2024-01-10 13:32:24 -0700

    changed the parameter scripts

--- Datalake/BA_Dimension/wf_Store_Data/deployment/0_Store_Data_Parameters.sql
+++ Datalake/BA_Dimension/wf_Store_Data/deployment/0_Store_Data_Parameters.sql
@@ -1,7 +1,7 @@
 --before we insert the parameters into the database we need 
 --to remove all the other parameters that could be there 
 --from previous parameter iterations
-delete from dev_raw.parameter_config
+delete from qa_raw.parameter_config
 where
     parameter_file_name = 'wf_store_data';
 
@@ -20,7 +20,7 @@ values
         'wf_store_data',
         'm_store_data',
         'source_bucket',
-        'gs://petm-bdpl-prod-prep-p1-gcs-gbl/nas/masterdata/store_data/'
+        'gs://petm-bdpl-qa-prep-p1-gcs-gbl/nas/masterdata/store_data/'
     ),
     (
         'wf_store_data',
