62c8b40adb1ab9c3735d030bef99182f0dd63da5
carwilki <carwilki@gmail.com>
2024-01-16 09:17:09 -0700

    updated deploy config so that we can deploy

--- Datalake/BA_Dimension/wf_Store_Data/deployment/2_config_paramaters.py
+++ Datalake/BA_Dimension/wf_Store_Data/deployment/2_config_paramaters.py
@@ -4,7 +4,7 @@ raw = "raw"
 parameter_file_name = "wf_store_data"
 parameter_section = "m_store_data"
 parameter_key = "source_bucket"
-parameter_value = "gs://petm-bdpl-qa-prep-p1-gcs-gbl/nas/masterdata/store_data"
+parameter_value = "gs://petm-bdpl-prod-prep-p1-gcs-gbl/nas/masterdata/store_data"
 
 # DO NOT Execute this script twice which will create the duplicates and the workflow will fail
 insert_param_config(
@@ -12,7 +12,7 @@ insert_param_config(
 )
 
 parameter_key = "archive_bucket"
-parameter_value = "gs://petm-bdpl-qa-raw-p1-gcs-gbl/nas/masterdata/store_data/"
+parameter_value = "gs://petm-bdpl-prod-raw-p1-gcs-gbl/nas/masterdata/store_data/"
 
 insert_param_config(
     raw, parameter_file_name, parameter_section, parameter_key, parameter_value
